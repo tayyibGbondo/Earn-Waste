3873ee87f7af27388b731f57918f47b8e34edb96
Tayyib Gbondo <gbondotahib72@gmail.com>
2022-08-20 21:28:28 -0700

    final update on the product page

--- root/pages/scripts/product.js
+++ root/pages/scripts/product.js
@@ -1,12 +1,17 @@
 const product1 = document.querySelector('.buynow')
 const ordre_container = document.querySelector('.ordre-container')
 const body = document.body
-const sendBtn = document.querySelector('.send')
+const closeBtn = document.querySelector('.close')
 
 product1.addEventListener('click', () => {
-    ordre_container.style.display = 'block'
+   if( ordre_container.style.display = 'none'){
+    ordre_container.style.display = 'flex'
+    console.log('yes its contain')
+   }else{
+    console.log('its does not contain')
+   }
 })
 
-sendBtn.addEventListener('click',() => {
-    ordre_container.style.remove.display = 'block'
+closeBtn.addEventListener('click',() => {
+    ordre_container.style.display = 'none';
 })
